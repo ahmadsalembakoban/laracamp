5baae92e6f6868056af2181994d7c912a68bcbbf
ahmadsalembakoban <ahmad.salem88@gmail.com>
2023-03-30 04:04:25 +0700

    slicing login, checkout,success-checkout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+    <link rel="stylesheet" href="{{asset('css/main.css')}}" type="text/css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+
+    <title>Laracamp by BuildWith Angga</title>
+</head>
+
+<body>
+
+    @include('components.navbar')  
+
+    @yield('content')
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js "></script>
+
+</body>
